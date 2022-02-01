b148e8cf73594bff3328616ca6d90a5259706421
Forklein <giuseppepisani2@gmail.com>
2022-02-01 12:34:45 +0100

    Add ejs view engine and add config.env

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
+//Express
 const express = require('express');
+//For ENV
 const dotenv = require('dotenv');
+//For logs request
+const morgan = require('morgan');
+//Parse request to body parser
+const bodyParser = require('body-parser');
+
+const path = require('path');
 
 const app = express();
 
 dotenv.config({ path: 'config.env' })
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.port || 8080;
+
+app.use(morgan('tiny'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//set view engine
+app.set('view engine', 'ejs')
+// app.set('/views', path.resolve(__dirname, 'views/ejs'));
+
+//load assets
+app.use('/css', express.static(path.resolve(__dirname, 'assets/css')));
+//http://localhost:3000/css/style.css
+app.use('/img', express.static(path.resolve(__dirname, 'assets/img')));
+app.use('/js', express.static(path.resolve(__dirname, 'assets/js')));
 
 app.get('/', (req, res) => {
-    res.send(`I'm Here`);
+    res.render('index', { title: res.statusCode })
 })
 
-app.listen(3000, () => { console.log(`Server is running on http://localhost:${PORT}`) });
+app.listen(PORT, () => { console.log(`Server is running on http://localhost:${PORT}`) });
