b8a0e9dd5ae052602928fd7e78629ec88470506b
Forklein <giuseppepisani2@gmail.com>
2022-02-01 11:40:43 +0100

    Add dotenv for env

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
 const app = express();
+
+dotenv.config({ path: 'config.env' })
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
