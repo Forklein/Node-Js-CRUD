bbeee14f60803cd2b2ce7639a42000524cdc1d63
Forklein <giuseppepisani2@gmail.com>
2022-02-04 21:48:21 +0100

    add gitignore

--- server/services/render.js
+++ server/services/render.js
@@ -1,7 +1,17 @@
 const axios = require('axios');
 
+const getAllUsers = async () => {
+    try {
+        const res = await axios.get('http://localhost:3000/api/users');
+        return res.data;
+        return res;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 exports.indexRender = (req, res) => {
-    res.render('users', { users: });
+    res.render('users', { users: getAllUsers() });
 };
 
 exports.createRender = (req, res) => {
